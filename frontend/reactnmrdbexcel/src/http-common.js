43916059605475a80e2fd61198488cb04a9730fb
andreagia <andreagia71@gmail.com>
2022-11-04 15:38:03 +0100

    added http://localhost:8080

--- frontend/reactnmrdbexcel/src/http-common.js
+++ frontend/reactnmrdbexcel/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://141.2.128.223:8080",
+    //baseURL: "http://localhost:8080",
     headers: {
         "Content-type": "application/json"
     }
